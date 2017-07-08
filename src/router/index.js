1537f218b79d7a1792c0938f47602302081acbf8
MaxWithU <csl199254@gmail.com>
2017-07-09 03:21:23 +0800

    edit filename

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import index from '@/components'
 
 Vue.use(Router)
 
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: index
     }
   ]
 })
