e03670e847ff3aad96352a4a253878d270575692
MaxWithU <csl199254@gmail.com>
2017-07-16 02:40:28 +0800

    马的逻辑写入

--- src/components/action/Horse.js
+++ src/components/action/Horse.js
@@ -1,2 +1,21 @@
-export default function () {
+export default function (mtx, item) {
+  let a = new Set()
+  console.log(item)
+  function range (x, y) {
+    if (x > y) {
+      !(mtx[item.y][item.x + x / 2]) && a.add(`${item.x + x}_${item.y + y}`)
+    } else {
+      mtx[item.y + y / 2] && !mtx[item.y + y / 2][item.x + x] && a.add(`${item.x + x}_${item.y + y}`)
+    }
+  }
+  range(1, 2)
+  range(1, -2)
+  range(-1, 2)
+  range(-1, -2)
+  range(2, 1)
+  range(2, -1)
+  range(-2, 1)
+  range(-2, -1)
+  console.log(a)
+  return a
 }
--- src/components/action/Soldier.js
+++ src/components/action/Soldier.js
@@ -1,13 +1,34 @@
 export default function (mtx, item) {
-  let a = new Set([
-    `${item.x + 1}_${item.y}`,
-    `${item.x - 1}_${item.y}`,
-    `${item.x}_${item.y + 1}`,
-    `${item.x}_${item.y - 1}`
-  ])
-  console.log(a)
+  let a = new Set()
+  console.log('Soldier')
   switch (item.camp) {
-    case 'A': return a
-    case 'B': return a
+    case 'A': if (item.y < 5) {
+      a.add(`${item.x}_${item.y + 1}`)
+    } else if (item.y === 5) {
+      a.add(`${item.x}_${item.y + 1}`)
+      a.add(`${item.x + 1}_${item.y}`)
+      a.add(`${item.x - 1}_${item.y}`)
+    } else {
+      a.add(`${item.x}_${item.y - 1}`)
+      a.add(`${item.x}_${item.y + 1}`)
+      a.add(`${item.x + 1}_${item.y}`)
+      a.add(`${item.x - 1}_${item.y}`)
+    }
+      console.log(a)
+      return a
+    case 'B': if (item.y > 4) {
+      a.add(`${item.x}_${item.y - 1}`)
+    } else if (item.y === 4) {
+      a.add(`${item.x}_${item.y - 1}`)
+      a.add(`${item.x + 1}_${item.y}`)
+      a.add(`${item.x - 1}_${item.y}`)
+    } else {
+      a.add(`${item.x}_${item.y - 1}`)
+      a.add(`${item.x}_${item.y + 1}`)
+      a.add(`${item.x + 1}_${item.y}`)
+      a.add(`${item.x - 1}_${item.y}`)
+    }
+      console.log(a)
+      return a
   }
 }
