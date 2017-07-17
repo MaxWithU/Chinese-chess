a924a83be1ff64e961451bf9fc8fec93a1e9bfd0
MaxWithU <csl199254@gmail.com>
2017-07-18 00:38:42 +0800

    编辑士的逻辑

--- src/components/action/Cannon.js
+++ src/components/action/Cannon.js
@@ -1,3 +1,8 @@
-export default function () {
-
+export default function (mtx, item) {
+  let a = mtx[item.y].map((it, i) => `${i}_${item.y}`)
+  mtx.forEach((it, i) => {
+    a.push(`${item.x}_${i}`)
+  })
+  let b = new Set(a)
+  console.log(b)
 }
